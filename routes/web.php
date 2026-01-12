03d461a9f0e2edfd9d13b5dc496835a59abe7947
JustMorron <aliefandika30@gmail.com>
2026-01-12 17:52:52 +0700

    fix logout

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,4 @@ use App\Http\Controllers\AuthController;
 
 Route::get('/', [AuthController::class, 'index'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.process');
+Route::post('/logout', [AuthController::class, 'logot'])->name('logout');
